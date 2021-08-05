2bb895f5e478f604d6ee1e9dcfaf128e2dfdf84b
Hidran Arias <hidran@gmail.com>
2021-08-06 00:59:07 +0200

    adds state with hooks useState and useEffect

--- src/Clock.js
+++ src/Clock.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react';
 
 const Clock = ({ country, timezone }) => {
   const t = Date.now() + 3600 * timezone * 1000;
-  const date = new Date(t);
-  console.log(date, t, timezone);
+  const dateIni = new Date(t);
+
+  const [date, setDate] = useState(dateIni);
+  const [nation, setNation] = useState(country);
+
+  useEffect(() => {
+    const nations = ['CHINA', 'JAPAN', 'KOREA'];
+    const idx = Math.floor(Math.random() * 3);
+    setNation(nations[idx]);
+    const interval = setInterval(() => {
+      const time = date.getTime() + 1000;
+      setDate(new Date(time));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [date]);
+
   return (
     <h2>
-      In {country} is {date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}
+      In {nation} is {date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}
     </h2>
   );
 };
