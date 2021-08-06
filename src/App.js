419bdd5667bfc57d60f162f0aff001fe12b1b6d2
Hidran Arias <hidran@gmail.com>
2021-08-07 01:21:40 +0200

    changes date into a digital clock

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1> My first react app</h1>
+        <h1>WORLD CLOCK</h1>
       </div>
 
       <Clock show country="ITALY" timezone="2" />
-      <Clock country="RUSSIA" timezone="3" />
-      <Clock country="CUBA" timezone="-6" />
     </>
   );
 }
